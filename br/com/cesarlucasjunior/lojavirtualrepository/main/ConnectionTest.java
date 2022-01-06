53976ded99dc505affdfe242ec9038b1e6981403
Cesar Lucas F. E. Junior <cesarlucasjunior93@gmail.com.br>
2022-01-05 23:12:21 -0300

    Adding a method to remove records from the database and return the number of rows impacted.

--- br/com/cesarlucasjunior/lojavirtualrepository/main/ConnectionTest.java
+++ br/com/cesarlucasjunior/lojavirtualrepository/main/ConnectionTest.java
@@ -15,6 +15,7 @@ public class ConnectionTest {
 		try {
 			Connection connection = new ConnectionFactory().getConnectionDataBase();
 			System.out.println("Database connection established.");
+			deleteProductInDataBase(connection);
 			setProductInDataBase(connection);
 			getResultDataBase(connection);
 			connection.close();
@@ -48,5 +49,12 @@ public class ConnectionTest {
 			System.out.println("Um item foi cadastrado com sucesso!");
 			System.out.println("ID: " + resultSet.getInt(1));
 		}
+	}
+	
+	public static void deleteProductInDataBase(Connection connection) throws SQLException {
+		Statement statement = connection.createStatement();
+		statement.execute("DELETE FROM PRODUTO WHERE ID > 3");
+		Integer numberOfRowsImpacted = statement.getUpdateCount(); //Return the number of rows impacted.
+		System.out.println(numberOfRowsImpacted + " rows removed.");
 	}
 }
