c533f3591a7fcb5cad6dad8e421685c6494eb068
cesarlucasjunior <cesarlucasjunior93@gmail.com.br>
2022-01-12 10:28:07 -0300

    Including new domain and dao classes to save registers in new table.

---
+++ br/com/cesarlucasjunior/lojavirtualrepository/dto/CategoriaDAO.java
@@ -0,0 +1,32 @@
+package br.com.cesarlucasjunior.lojavirtualrepository.dto;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import br.com.cesarlucasjunior.lojavirtualrepository.model.Categoria;
+
+public class CategoriaDAO {
+
+	private Connection connection;
+	
+	public CategoriaDAO(Connection connection) {
+		this.connection = connection;
+	}
+	
+	public void save(Categoria categoria) throws SQLException {
+		try(PreparedStatement preparedStatement = this.connection.prepareStatement("INSERT INTO CATEGORIA (NOME) VALUES (?)", Statement.RETURN_GENERATED_KEYS)){
+			preparedStatement.setString(1, categoria.getNome());
+			
+			preparedStatement.execute();
+			
+			try(ResultSet resultSet = preparedStatement.getGeneratedKeys()){
+				while(resultSet.next()) {
+					System.out.println("ID DA CATEGORIA GERADA: " + resultSet.getInt("ID"));
+				}
+			}
+		}
+	}
+}
